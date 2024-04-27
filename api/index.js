339cc48284861eab899b9433b2a82d0aefc1f95c
pula-ashok <ashokpula0307@gmail.com>
2024-04-27 16:53:26 +0530

    signup and error handling

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import { conncectDB } from "./db/createConnection.js";
-import useRouter from "./routes/user.router.js";
+import useRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 const app = express();
 
+app.use(express.json());
 dotenv.config();
 
 app.listen(3000, () => {
@@ -12,7 +14,17 @@ app.listen(3000, () => {
   console.log(`server is running at port 3000`);
 });
 app.use("/api/user", useRouter);
+app.use("/api/auth", authRouter);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    error: message,
+    statusCode: statusCode,
+  });
+});
 // app.get("/", (req, res) => {
 //   res.json({ message: "test api working" });
 // });
